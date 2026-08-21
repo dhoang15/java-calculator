9604ba36fb428d9d8cbe60f9183cf25d7ad647f3
Võ Nguyễn Duy Hoàng <hduy64770@gmail.com>
2026-08-22 02:40:06 +0700

    Uncomment calculator model tests in main method

--- src/test/test.java
+++ src/test/test.java
@@ -5,34 +5,34 @@ import view.CaculatorView;
 
 public class test {
     public static void main(String[] args) {
-        // CaculatorModel s1 = new CaculatorModel();
-        // // kiem tra cong
-        // s1.setFirstValue(4);
-        // s1.setSecondValue(2);
-        // s1.sum();
-        // System.out.println(s1.getResult());
-        // s1.clear();
-        // // Kiem tra tru
-        // s1.setFirstValue(4);
-        // s1.setSecondValue(2);
-        // s1.sub();
-        // System.out.println(s1.getResult());
-        // s1.clear();
-        // // Kiem tra nhan
-        // s1.setFirstValue(4);
-        // s1.setSecondValue(2);
-        // s1.mul();
-        // System.out.println(s1.getResult());
-        // s1.clear();
-        // // Kiem tra chia
-        // s1.setFirstValue(4);
-        // s1.setSecondValue(2);
-        // try {
-        //     s1.div();
-        //     System.out.println(s1.getResult());
-        // } catch (Exception e) {
-        //     System.out.println(e.getMessage());
-        // }
+        CaculatorModel s1 = new CaculatorModel();
+        // kiem tra cong
+        s1.setFirstValue(4);
+        s1.setSecondValue(2);
+        s1.sum();
+        System.out.println(s1.getResult());
+        s1.clear();
+        // Kiem tra tru
+        s1.setFirstValue(4);
+        s1.setSecondValue(2);
+        s1.sub();
+        System.out.println(s1.getResult());
+        s1.clear();
+        // Kiem tra nhan
+        s1.setFirstValue(4);
+        s1.setSecondValue(2);
+        s1.mul();
+        System.out.println(s1.getResult());
+        s1.clear();
+        // Kiem tra chia
+        s1.setFirstValue(4);
+        s1.setSecondValue(2);
+        try {
+            s1.div();
+            System.out.println(s1.getResult());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
         // Kiem tra giao dien
         new CaculatorView();
     }
