5f8a154b3f003455eb498269dee100779dbe5fdf
dhoang15 <hduy64770@gmail.com>
2026-08-21 19:05:57 +0700

    Thêm các nút cho máy tính

--- src/MyCaculator.java
+++ src/MyCaculator.java
@@ -5,6 +5,24 @@ public class MyCaculator extends JFrame {
         this.setTitle("Caculator");
         this.setSize(300,300);
         this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JTextField jtextfeild = new JTextField();
+        JButton jbutton = new JButton("0");
+        JButton jbutton1 = new JButton("1");
+        JButton jbutton2 = new JButton("2");
+        JButton jbutton3 = new JButton("3");
+        JButton jbutton4 = new JButton("4");
+        JButton jbutton5 = new JButton("5");
+        JButton jbutton6 = new JButton("6");
+        JButton jbutton7 = new JButton("7");
+        JButton jbutton8 = new JButton("8");
+        JButton jbutton9 = new JButton("9");
+        JButton jbutton_cong = new JButton("+");
+        JButton jbutton_tru = new JButton("-");
+        JButton jbutton_nhan = new JButton("*");
+        JButton jbutton_chia = new JButton("/");
+        JButton jbutton_bang = new JButton("=");
+        
         this.setVisible(true);
     }
     public static void main(String[] args){
